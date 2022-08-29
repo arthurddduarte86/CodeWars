ae32b7e0483ea58bf8f4e63abee12753c7d780da
arthur duarte <arthurddduarte86@gmail.com>
2022-08-29 13:38:24 -0300

    first commit

---
+++ Code-Py/Multiply.py
@@ -0,0 +1,19 @@
+#Instructions
+#This code does not execute properly. Try to figure out why.
+
+#Solution
+def multiply(a, b):
+    return a * b
+
+#Sample Tests
+"""
+import codewars_test as test
+from solution import multiply
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(multiply(2,1), 2)
+        
+"""
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Readme com informações e catálogo do meu processo na plataforma CodeWars para Python
+
+## Sobre
+Sejam bem vindos, este é o catálogo de exercícios e códigos para a linguagem Python na plataforma CodeWars.
+
+# Kata
+## Rank:8 kyu
+
+* [Multiply]() [-> Site CodeWars](https://www.codewars.com/kata/50654ddff44f800200000004/train/python) 
+
