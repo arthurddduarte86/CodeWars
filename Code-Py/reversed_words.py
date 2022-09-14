322b168707d1fd18733e1a793e87fcda63d41eb7
arthur duarte <arthurddduarte86@gmail.com>
2022-09-13 22:23:28 -0300

    news

---
+++ Code-Py/reversed_words.py
@@ -0,0 +1,17 @@
+#Complete the solution so that it reverses all of the words
+# within the string passed in.
+
+#Example(Input --> Output):
+
+#"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+#
+#
+def reverse_words(s):
+    s = s.split(" ")
+    return " ".join(s[len(s)-1::-1])
+#
+#
+#
+def reverse_words(s):
+    s = s.split(" ")
+    return " ".join(s[::-1])
