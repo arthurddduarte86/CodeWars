9351b8e551454d08b7520b95f8b6297993d208cf
Arthur Duarte <arthurddduarte86@gmail.com>
2022-09-19 17:59:39 -0300

    new

---
+++ Code-Py/Square_Every_Digit.py
@@ -0,0 +1,15 @@
+'''
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+'''
+#
+#
+def square_digits(num): 
+    result = "".join(f'{int(value)**2}' for value in str(num))
+    return int(result)
+# First of all, need to convert (num) to string, because integer values
+# are not iterable in python.
+
