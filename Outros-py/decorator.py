d0095557e02a10e415f06232a68803f40cc781b2
Arthur Duarte <arthurddduarte86@gmail.com>
2022-11-01 20:11:20 -0300

    new

---
+++ Outros-py/decorator.py
@@ -0,0 +1,21 @@
+# ESTUDO SOBRE DECORATOR
+
+def check(func):
+    def inside(a, b):
+        if b == 0:
+            print("Não é possível divisão por 0")
+            
+        func(a, b)
+    return inside
+
+
+
+@check
+def div(a, b):
+    print(a / b)
+
+# O DECORATOR é o mesmo que a linha abaixo
+#div = check(div)
+#
+#
+div(10, 1)
