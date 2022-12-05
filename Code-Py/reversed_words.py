701f414f63a835f6456d4c3004c0077585bc58bf
Arthur Duarte <arthurddduarte86@gmail.com>
2022-12-05 16:18:51 -0300

    new

--- Code-Py/reversed_words.py
+++ Code-Py/reversed_words.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 #Complete the solution so that it reverses all of the words
-# within the string passed in.
+#within the string passed in.
 
 #Example(Input --> Output):
 
@@ -14,23 +13,9 @@ def reverse_words(s):
 #
 #
 def reverse_words(s):
-    s = s.split(" ")
-=======
-#Complete the solution so that it reverses all of the words
-# within the string passed in.
-
-#Example(Input --> Output):
-
-#"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+    return " ".join(reversed(s.split()))
 #
 #
-def reverse_words(s):
-    s = s.split(" ")
-    return " ".join(s[len(s)-1::-1])
-#
-#
-#
-def reverse_words(s):
-    s = s.split(" ")
->>>>>>> refs/remotes/origin/main
-    return " ".join(s[::-1])
+#    
+def reverseWords(str):
+    return " ".join(str.split(" ")[::-1])
